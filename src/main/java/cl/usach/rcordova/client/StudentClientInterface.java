01fd508fe73667bb84a106ab4805deae8e0144e6
koke <koke@192.168.0.22>
2017-03-19 03:19:58 -0300

    Segundo

---
+++ src/main/java/cl/usach/rcordova/client/StudentClientInterface.java
@@ -0,0 +1,17 @@
+package cl.usach.rcordova.client; 
+
+import javax.ws.rs.core.Response;
+
+public interface StudentClientInterface {
+
+	public Response getSignup();
+
+	public Response postSignup(String userName, String password,
+			String firstName, String lastName, String dateOfBirth,
+			String emailAddress) throws Exception;
+
+	public Response getLogin();
+
+	public Response postLogin(String userName, String password);
+
+}
